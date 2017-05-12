16125f659b283c168d70aa59e3f26708573a4c1c
Alexey <alsifero@gmail.com>
2017-05-12 22:19:57 +0300

    migrate to beta4 pacifySdk

--- app/src/main/java/com/masslight/pacifysdk/sample/MainActivity.java
+++ app/src/main/java/com/masslight/pacifysdk/sample/MainActivity.java
@@ -1,20 +1,23 @@
 package com.masslight.pacifysdk.sample;
 
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.masslight.lib.pacifysdk.core.model.Color;
 import com.masslight.lib.pacifysdk.core.model.Token;
 import com.masslight.lib.pacifysdk.sdk.PacifySdk;
+import com.masslight.lib.pacifysdk.sdk.model.PacifyEnvironment;
 import com.masslight.lib.pacifysdk.sdk.model.entity.Coupon;
+import com.masslight.lib.pacifysdk.sdk.model.entity.PacifyAppearance;
 import com.masslight.lib.pacifysdk.sdk.model.entity.PacifySettings;
+import com.masslight.lib.pacifysdk.sdk.model.entity.PacifySupportInfo;
 import com.masslight.lib.pacifysdk.sdk.model.entity.PacifyUserData;
 import com.masslight.lib.pacifysdk.sdk.model.entity.TokensInfo;
-
-import java.util.Date;
 
 import sample.pacifysdk.masslight.com.pacifysdkandroidsample.R;
 
@@ -22,9 +25,10 @@ public final class MainActivity extends AppCompatActivity implements PacifySdk.P
 
     private static final String TAG = MainActivity.class.getCanonicalName();
 
-    private static final String SAMPLE_API_KEY = "test-3-org-api-key";
-    private static final String SAMPLE_USER_SESSION_TOKEN = "valid_1001";
-    private static final String SAMPLE_COUPON = "1ZKTO9AH";
+    //    private static final String SAMPLE_API_KEY = "test-3-org-api-key";
+    private static final String SAMPLE_API_KEY = "8271740bb0ded5e1ff42258c794920e7"; //medela staging
+    private static final String SAMPLE_USER_SESSION_TOKEN = "valid_1234";
+    private static final String SAMPLE_COUPON = "6UA4CSIV";
 
     private EditText apiKeyField;
     private EditText tokenField;
@@ -82,27 +86,41 @@ public final class MainActivity extends AppCompatActivity implements PacifySdk.P
     }
 
     private void launchVideoConference() {
-        TokensInfo tokensInfo = new TokensInfo(
-                new Token(editTextContent(apiKeyField)),
-                new Token(editTextContent(tokenField))
+        final PacifyUserData pacifyUserData = new PacifyUserData(
+                "Mister Twister",
+                "Bos", // optional, used for payment only
+                "mistertwister@matrix.com"
         );
 
-        PacifyUserData userData = PacifyUserData.builder()
-                .withFirstName("Nick")
-                .withLastName("Bos")
-                .withEmail("nick@somedomain.com")
-                .withPhone("1234567890")
-                .withDateOfBirth(new Date())
-                .build();
+        final TokensInfo tokensInfo = new TokensInfo(
+                new Token(apiKeyField.getText().toString()),
+//                new Token("valid_1001")
+                new Token(tokenField.getText().toString())
+        );
 
-        Coupon coupon = new Coupon(editTextContent(couponField));
+        final PacifySupportInfo pacifySupportInfo = new PacifySupportInfo("support@company.com", "1234567890");
+        final PacifyAppearance pacifyAppearance = new PacifyAppearance(
+                Color.ofRgb(249.0 / 255, 249.0 / 255, 249.0 / 255),
+                Color.ofRgb(253.0 / 255, 217.0 / 255, 123.0 / 255),
+                Color.BLACK.withAlphaComponent(0.8),
+                Color.ofRgb(143.0 / 255, 98.0 / 255, 146.0 / 255),
+                Color.WHITE,
+                Color.ofRgb(76.0 / 255, 0, 82.0 / 255),
+                ContextCompat.getDrawable(getApplicationContext(), R.drawable.medela_logo)
+        );
+        final PacifySettings pacifySettings = new PacifySettings(
+                pacifyAppearance,
+                PacifyEnvironment.Testing,
+                pacifySupportInfo,
+                "SampleApp"
+        );
 
         PacifySdk.call(
                 MainActivity.this,
                 tokensInfo,
-                coupon,
-                userData,
-                PacifySettings.empty(),
+                Coupon.create(SAMPLE_COUPON),
+                pacifyUserData,
+                pacifySettings,
                 this
         );
     }
